693100574815ebc8233596f284d5e4e7027eed9d
Darinel Escobar <darinel.escobar@outlook.com>
2026-03-31 16:26:02 -0600

    feat(i18n): add language provider and localized content adapter

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "@/assets/styles/globals.css";
+import { LanguageProvider } from "@/context/LanguageProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <LanguageProvider>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </LanguageProvider>
   </StrictMode>
 );
